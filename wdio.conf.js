da43921aa200d44edf8490e211abb8339b3440a8
Fahad Miraj <fahad.miraj98@gmail.com>
2023-04-11 03:00:31 +0500

    using chrome driver

--- wdio.conf.js
+++ wdio.conf.js
@@ -118,7 +118,7 @@ export const config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [/*'chromedriver','geckodriver',*/'selenium-standalone'],
+    services: ['chromedriver'/*,'geckodriver','selenium-standalone'*/],
     /*services: [
         [
             'geckodriver',
